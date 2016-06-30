48ae93254c00d62795b4ec617238c09b5cd02748
Guillaume Massard <llomgui@live.fr>
2016-06-30 07:39:49 +0000

    Adding cmd dev memory

--- commands/dev.php
+++ commands/dev.php
@@ -14,6 +14,22 @@ $dev = function ($who, $message, $type) {
 			reloadExtensions();
 			$bot->network->sendMessageAutoDetection($who, 'Extensions reloaded!', $type);
 			break;
+
+		case 'memory':
+			$memory = [
+				'Bits'      =>round(memory_get_usage(true) * 8), 
+				'Bytes'     =>memory_get_usage(true), 
+				'Kilobytes' =>round(memory_get_usage(true) / 1024), 
+				'Megabytes' =>round(memory_get_usage(true) / 1024 / 1024)
+			];
+
+			$temp = [];
+			foreach ($memory as $key => $val) {
+				array_push($temp, $key . ': ' . $val);
+			}
+
+			$bot->network->sendMessageAutoDetection($who, implode(' | ', $temp), $type);
+			break;
 	}
 
 };
