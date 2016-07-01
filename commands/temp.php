188301c44dc9ad5944de412807082324ed07d27d
Guillaume Massard <llomgui@live.fr>
2016-07-01 12:13:15 +0000

    Adding temp command

---
+++ commands/temp.php
@@ -0,0 +1,52 @@
+<?php
+
+$temp = function ($who, $message, $type) {
+
+	$bot = actionAPI::getBot();
+
+	if( empty($message[1]) || empty($message[2])       ||
+		empty($message[3]) || !is_numeric($message[3]) ||
+		$message[3] < 0    || $message[3] > 24) {
+
+		return $bot->network->sendMessageAutoDetection($who, 'Usage: !temp [mem/mod/own] [xatid/regname] [time(0-24)]', $type);
+	}
+
+	if (is_numeric($message[2]) && isset($bot->users[$message[2]])) {
+		$user = $bot->users[$message[2]];
+	} else {
+		foreach($bot->users as $id => $object) {
+			if (is_object($object)) {
+				if (strtolower($object->getRegname()) == strtolower($message[2])) {
+					$user = $object;
+					break;
+				}
+			}
+		}
+	}
+
+	if(isset($user)) {
+
+		$message[1] = strtolower($message[1]);
+		switch($message[1]) {
+			case 'mem':
+			case 'member':
+			case 'membre':
+				$bot->network->sendPrivateConversation($user->getID(), '/mb' . $message[3]);
+			break;
+
+			case 'mod':
+			case 'moderator':
+			case 'moderateur':
+				$bot->network->sendPrivateConversation($user->getID(), '/m' . $message[3]);
+			break;
+
+			case 'own':
+			case 'owner':
+				$bot->network->sendPrivateConversation($user->getID(), '/mo' . $message[3]);
+			break;
+		}
+		
+	} else {
+		$bot->network->sendMessageAutoDetection($who, 'User is not here', $type);
+	}
+};
