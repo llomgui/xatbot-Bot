29f4d6459b5f22e6ba64db53d269d872d6c12f60
xStah <stahpizzaa@gmail.com>
2016-07-02 15:00:20 +0000

    Random user command

---
+++ commands/randomuser.php
@@ -0,0 +1,9 @@
+<?php
+
+$randomuser = function ($who, $message, $type) {
+
+	$bot = actionAPI::getBot();
+	
+	$random = array_rand($bot->users);
+	$bot->network->sendMessageAutoDetection($who, $bot->users[$random]->getNick(), $type);
+};
