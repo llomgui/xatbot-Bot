38e99d9dbe1246c0212b36a7e7cda3cafa3d8b7c
Tehchy <juniortec21@yahoo.com>
2016-07-04 11:34:06 -0400

    Added 3 new commands
    New commands:
    Wikipedia
    Slots
    Randomsmiley

---
+++ commands/wikipedia.php
@@ -0,0 +1,24 @@
+<?php
+
+$wikipedia = function ($who, $message, $type) {
+	$bot = actionAPI::getBot();
+	
+	unset($message[0]);
+	$message = implode(' ', $message);
+	
+	if (empty($message)) {
+		return $bot->network->sendMessageAutoDetection($who, 'You\'re not searching for anything (confused#)', $type);
+	}
+	$page = file_get_contents('http://en.wikipedia.org/w/api.php?action=opensearch&search=' . urlencode($message) . '&format=xml&limit=1');
+	if (!$page) {
+		return $bot->network->sendMessageAutoDetection($who, 'I can\'t reach wikipedia.org at this monent, please try again later.', $type);
+	}
+	
+	$xml = simplexml_load_string($page);
+	if ((string)$xml->Section->Item->Description) {
+		$wiki = "Wikipedia page: http://en.wikipedia.org/wiki/" . (string)$xml->Section->Item->Text;
+	} else {
+		$wiki = "Wikipedia page could not be found.";
+	}
+	$bot->network->sendMessageAutoDetection($who, $wiki, $type);
+};
