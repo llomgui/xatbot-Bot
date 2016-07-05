89b6065bdfdb350fe742222a3f235b701494ae60
Tehchy <juniortec21@yahoo.com>
2016-07-05 09:29:17 -0400

    New command
    Added Choose command

---
+++ commands/choose.php
@@ -0,0 +1,20 @@
+<?php
+
+$choose = function ($who, $message, $type) {
+
+	$bot = actionAPI::getBot();
+
+	if (!isset($message[1]) || empty($message[1])) {
+		return $bot->network->sendMessageAutoDetection($who, 'Usage: !store [allpower/everypower/power]', $type);
+	}
+	unset($message[0]);
+	$message = implode(' ', $message);
+	$message = preg_split('/ or /', strtolower($message), 2);
+	
+	if ((!isset($message[0]) || empty($message[0])) || (!isset($message[1]) || empty($message[1]))) {
+		return $bot->network->sendMessageAutoDetection($who, 'Usage: !choose [first choice] or [second choice]', $type);
+	}
+		
+	$choice = rand(0, 100) > 50 ? $message[0] : $message[1];	
+	$bot->network->sendMessageAutoDetection($who, 'I have chosen \'' . $choice. '\'.', $type);
+};
