5dfb3d60676662143a0ac76a426d2e563182317d
Tehchy <juniortec21@yahoo.com>
2016-07-06 10:50:52 -0400

    Limiting things + new stuff

--- commands/joke.php
+++ commands/joke.php
@@ -3,8 +3,9 @@
 $joke = function ($who, $message, $type) {
     
     $bot = actionAPI::getBot();
-	
-    $page = @file_get_contents('http://www.jokesclean.com/OneLiner/Random/');
+    
+	$stream = stream_context_create(['http'=> ['timeout' => 1]));
+    $page = @file_get_contents('http://www.jokesclean.com/OneLiner/Random/',false, $stream);
 	if (!$page) {
         return $bot->network->sendMessageAutoDetection($who, 'I am unable to grab a joke at this moment, please try again later.', $type);
     }
--- modules/onChat.php
+++ modules/onChat.php
@@ -1,5 +1,29 @@
 <?php
 
 $onChat = function ($array) {
+    $chatInfo = explode(';=', $array['b']);
+    /*
+        0 = background
+        1 = tabbed chat
+        2 = tabbed chat id
+        3 = language
+        4 = radio
+        5 = button color
+    */
+    if (isset($array['r'])) {
+        switch (intval($array['r'])) {
+            case 1: // Main
+                break;
+            case 2: // Mod
+                break;
+            case 3: // Member
+                break;
+            case 4: // Owner
+                break;
+            default: // Guest
+                break;
+        }
+    }
     
+    //Set Bot rank
 };
