795ac33b6b62c63bd58f5f193c68bc6e090f1228
Tehchy <juniortec21@yahoo.com>
2016-07-06 12:08:40 -0400

    spacing

--- commands/joke.php
+++ commands/joke.php
@@ -4,7 +4,7 @@ $joke = function ($who, $message, $type) {
     
     $bot = actionAPI::getBot();
     
-	 $stream = stream_context_create(['http'=> ['timeout' => 1]]);
+    $stream = stream_context_create(['http'=> ['timeout' => 1]]);
     $page = @file_get_contents('http://www.jokesclean.com/OneLiner/Random/',false, $stream);
 	if (!$page) {
         return $bot->network->sendMessageAutoDetection($who, 'I am unable to grab a joke at this moment, please try again later.', $type);
