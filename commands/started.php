d5fce3ec386f880a0587c1f0a29b21008bbbea0b
Tehchy <juniortec21@yahoo.com>
2016-07-11 11:06:05 -0400

    fixed commands.json and changed started

--- commands/started.php
+++ commands/started.php
@@ -6,10 +6,5 @@ $started = function ($who, $message, $type) {
 	
 	$started = time() - $bot->started;
 	
-	$days = floor($started / 86400);
-	$hours = floor($started / 3600);
-	$minutes = floor(($started / 60) % 60);
-	$seconds = $started % 60;
-	
-	$bot->network->sendMessageAutoDetection($who, 'I was started ' . sprintf("%02d days, %02d hours, %02d minutes and %02d seconds", $days, $hours, $minutes, $seconds) . ' ago.', $type);
+	$bot->network->sendMessageAutoDetection($who, 'I was started ' . $bot->secondsToTime($started) . ' ago.', $type);
 };
