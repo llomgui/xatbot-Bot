72e07f51b7c4e04bb171512ca9b992b467470f5d
Tehchy <juniortec21@yahoo.com>
2016-07-11 10:41:23 -0400

    changed active

--- commands/active.php
+++ commands/active.php
@@ -7,9 +7,5 @@ $active = function ($who, $message, $type) {
     $userTime = $now - dataAPI::get($who . '_active');
     $displayName = $bot->users[$who]->isRegistered() ? $bot->users[$who]->getRegname() . '(' . $bot->users[$who]->getID() . ')'  : $bot->users[$who]->getID();
 
-    $hours = floor($userTime / 3600);
-    $minutes = floor(($userTime / 60) % 60);
-    $seconds = $userTime % 60;
-
-    $bot->network->sendMessageAutoDetection($who, $displayName . ' has been at this chat (while I was here) for: ' . sprintf("%02d hours, %02d minutes and %02d seconds", $hours, $minutes, $seconds), $type);
+    $bot->network->sendMessageAutoDetection($who, $displayName . ' has been at this chat (while I was here) for: ' . $bot->secondsToTime($userTIme), $hours, $minutes, $seconds), $type);
 };
