4de711735c177908eb14cbb8ba60e45671155a69
Tehchy <juniortec21@yahoo.com>
2016-08-18 08:33:38 -0400

    stuff

--- commands/test.php
+++ commands/test.php
@@ -7,18 +7,18 @@ $test = function ($who, $message, $type) {
     
     $message = implode(' ', $message);
     
-	if (empty($message)) {
+    if (empty($message)) {
         return $bot->network->sendMessageAutoDetection($who, 'The message cannot be empty.', $type, true);
-	}
-	//remove parentheses, replace all #'s & alt255 with a space
+    }
+    //remove parentheses, replace all #'s & alt255 with a space
     //not using regex because people like to use emojis/symbols with num
-	$message = trim(str_replace(array('(',')','#',' '/*<-alt255*/), ' ', $message));	
-	$message = explode(' ', $message);
-	$message = array_filter($message, 'strlen');
+    $message = trim(str_replace(array('(',')','#',' '/*<-alt255*/), ' ', $message));	
+    $message = explode(' ', $message);
+    $message = array_filter($message, 'strlen');
 	
-	if (count($message) < 1) {
+    if (count($message) < 1) {
         return $bot->network->sendMessageAutoDetection($who, 'The message cannot be empty.', $type, true);
     }
     
-	$bot->network->sendMessageAutoDetection($who, 'Test: (' . implode('#', $message) . '#)', $type);
+    $bot->network->sendMessageAutoDetection($who, 'Test: (' . implode('#', $message) . '#)', $type);
 };
