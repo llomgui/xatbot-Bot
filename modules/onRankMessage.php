384dbd973748b6a03b1de9b3f18c8a0cb964b09a
Tehchy <juniortec21@yahoo.com>
2016-08-22 12:23:21 -0400

    new handler
    added rankmessage handler
    fixed oldmessage handler

---
+++ modules/onRankMessage.php
@@ -0,0 +1,40 @@
+<?php
+
+$onRankMessage = function ($who, $message, $reason, $array) {
+    $bot = actionAPI::getBot();
+    
+    if ($message[0] == "/") {
+        $action = str_replace(range(0,9), '', $message);
+        $duration = substr($message, strlen($action) - 1);
+        switch (substr($action, 0, 2)) {
+            case '/u': //unban
+                if (substr_count($reason, ',') > 0) {// gameban
+                    $reason  = explode(',', $reason);
+                    $game    = $reason[0];
+                    $seconds = $reason[1];
+                    $hours   = $reason[2];
+                    $powers  = xatVariables::getPowers();
+                    
+                    /* 
+                        TODO
+                        Save highscores to database
+                    */
+                } else {
+                    //regular unban
+                }
+                break;
+            case '/g': //ban
+                if (isset($array['w'])) {
+                    //game banned
+                }
+                return; //for now
+                break;
+            case '/m': //change rank (mem,guest,mod,owner)
+                return; //for now
+                break;
+            default:
+                return;
+                break;
+        }
+    }
+};
