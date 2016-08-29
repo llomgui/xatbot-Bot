e8d5d2ee4209a7eff52bb54fbcb1c87356eb0b97
Tehchy <juniortec21@yahoo.com>
2016-08-29 17:54:25 -0400

    lots of changes to jinx
    fixed everything no more errors :+1:

--- commands/jinx.php
+++ commands/jinx.php
@@ -12,48 +12,41 @@ $jinx = function ($who, $message, $type) {
     
     $message = implode(' ', $message);
     
-    $ChkSum = function($string) {
-        $res = 0;
-        for($i = 0; $i < strlen($string); $i++) {
-            $charcode = ord($string[$i]);
-            if ($charcode != 32) {
-                $res += $charcode;
-            }
-        }
-        return $res;  
-    };
+    $string = $message . $who;
+    $Rand = 0;
+    for ($i = 0; $i < strlen($string); $i++) {
+        $Rand += ord($string[$i]) != 32 ? ord($string[$i]) : 0;
+    }
     
-    $randomSort = function($s) {
-        if (($s & 1)){
-            return (-1);
-        };
-        return (1);
-    };
+    $seed = $Rand;
 
-    $seed = $Rand = $ChkSum($message . $who);
-    $seed = ($seed ^ ($seed << 21));
+    $random = function() use (&$seed){
+        $seed = ($seed ^ ($seed << 21));
+        
+        $a = $seed;
+        $b = 35;
+        $z = hexdec(80000000); 
+        if ($z & $a) { 
+            $a = ($a >> 1); 
+            $a &= (~$z); 
+            $a |= 0x40000000; 
+            $a = ($a >> ($b - 1)); 
+        } else { 
+            $a = ($a >> $b); 
+        } 
+        
+        $seed = ($seed ^ ($a));
+        $seed = ($seed ^ ($seed << 4));
+        return ($seed);
+    };
     
-    $a = $seed;
-    $b = 35;
-    $z = hexdec(80000000); 
-    if ($z & $a) { 
-        $a = ($a >> 1); 
-        $a &= (~$z); 
-        $a |= 0x40000000; 
-        $a = ($a >> ($b - 1)); 
-    } else { 
-        $a = ($a >> $b); 
-    } 
-    
-    $seed = ($seed ^ ($a));
-    $seed = ($seed ^ ($seed << 4));
     $Arg = 100 * (4 / 50);
     
     $message = explode(' ', $message);
     $_local_9 = [];
 
-    for($i = 0; $i < count($message); $i++) {
-        if ($message[$i][0] == "(" && $message[$i][count($message[$i]) - 1] == ")"){
+    for ($i = 0; $i < count($message); $i++) {
+        if ($message[$i][0] == "(" && end($message[$i]) == ")"){
             $_local_9[] = $message[$i];
             $message[$i] = ">";
         };
@@ -61,52 +54,62 @@ $jinx = function ($who, $message, $type) {
     
     switch (strtolower($jinxType)) {//JinxIt
         case "reverse":
-            for($i = 0; $i < count($message); $i++) {
+            for ($i = 0; $i < count($message); $i++) {
                 $message[$i] = strrev($message[$i]);
             }
             break;
         case "mix":
         default:
-            for($i = 0; $i < count($message); $i++) {
+            for ($i = 0; $i < count($message); $i++) {
                 $message[$i] = str_shuffle($message[$i]);
             }
             break;
         case "ends":
             $message2 = [];
             $messageTmp = "";
-            for($i = 0; $i < count($message); $i++) {
+            
+            for ($i = 0; $i < count($message); $i++) {
                 $message2 = str_split($message[$i]);
                 $messageTmp = $message2[0];
-                $message2[0] = $message2[count($message2) - 1];
+                $message2[0] = end($message2);
                 $message2[count($message2) - 1] = $messageTmp;
-                $message[$i] = implode("", $message2);
+                $message[$i] = implode($message2);
             }
             break;
         case "middle":
             $message2 = $message3 = [];
             $messageTmp = $messageTmp2 = "";
-            for($i = 0; $i < count($message); $i++) {
+            
+            for ($i = 0; $i < count($message); $i++) {
                 $message2 = str_split($message[$i]);
                 if (count($message2) > 3) {
                     $messageTmp = $message2[0];
-                    $messageTmp2 = $message2[count($message2) - 1];
+                    $messageTmp2 = end($message2);
                     $message3 = array_slice($message2, 1, count($message2) - 1);
-                    $message3 = sort($message3, $randomSort($seed));
-                    $message3 = array_unshift($message3, $messageTmp);
+                    
+                    usort($message3, function($a, $b) use (&$random) {
+                        return ($random() & 1) ? -1 : 1;
+                    });   
+                    
+                    array_unshift($message3, $messageTmp);
                     $message3[] = $messageTmp2;
-                    $message[$i] = implode("", $message3);
+                    $message[$i] = implode($message3);
                 }
             }
             break;
         case "hang":
             $messageTmp = implode(" ", $message);
             $message2 = preg_replace("/[ >]/", "", $messageTmp);
-            $message2 = str_split($message2);
-            $Arg = $Arg > count($message2) ? count($message2) : $Arg;
             
-            for($i = 0; $i < $Arg; $i++) {
-                $messageTmp = implode("_", explode($message2[$seed % count($message2)], $messageTmp));
+            if (strlen($message2 > 0)) {
+                $message2 = str_split($message2);
+                $Arg = $Arg > count($message2) ? count($message2) : $Arg;
+                
+                for ($i = 0; $i < $Arg; $i++) {
+                    $messageTmp = implode("_", explode($message2[$random() % count($message2)], $messageTmp));
+                }
             }
+            
             $message = explode(" ", $messageTmp);
             break;
         case "egg":
@@ -126,7 +129,7 @@ $jinx = function ($who, $message, $type) {
             break;
     }
     
-    for($i = 0; $i < count($message); $i++) {
+    for ($i = 0; $i < count($message); $i++) {
         if ($message[$i] == ">"){
             $message[$i] = array_pop($_local_9);
         }
