f368d45ec65eb1b17fdcc8bbde349be9f7f3308b
Guillaume Massard <llomgui@live.fr>
2016-09-15 13:31:31 +0000

    Removing debug

--- commands/say.php
+++ commands/say.php
@@ -3,10 +3,6 @@
 $say = function ($who, $message, $type) {
     $bot = actionAPI::getBot();
 
-    var_dump($bot->minranks);
-
-    var_dump($bot->flagToRank($who));
-
     if (!$bot->minrank($who, 'say')) {
         return $bot->network->sendMessageAutoDetection($who, 'Sorry you do not have enough rank to use this command!', $type);
     }
