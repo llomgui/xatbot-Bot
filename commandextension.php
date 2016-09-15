b999fab18adebcaca0d5803da0c41cefdc13e0e1
Skyleter <mogok1penguin@gmail.com>
2016-09-14 21:28:26 -0300

    **Made for testing**
    Testing a new feature yet. However, will use this myself. Not sure if jedi approves it.

---
+++ commandextension.php
@@ -0,0 +1,9 @@
+<?php				     
+	$commandsfile = json_decode(file_get_contents('./config.json', true), true);
+
+	$cmdextension    = $data['bots']['cmd-extension'];
+	
+	$cmdxs = $data['bots']['cmd-extension']; // short, using for creating commands.
+	
+	//made for testing by skyleter
+?>
