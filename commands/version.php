42c9009127bb6b865cf51e64c76022d5120694d2
Yoiker <idcarros@gmail.com>
2017-03-22 06:47:56 -0400

    Version.php
    This is to know the bot version.

--- commands/version.php
+++ commands/version.php
@@ -4,5 +4,5 @@ $version = function (int $who, array $message, int $type) {
 
     $bot = actionAPI::getBot();
 	
-    $bot->network->sendMessageAutoDetection($who, 'Script Version is: [03/21/2017]', $type);
+    $bot->network->sendMessageAutoDetection($who, 'Script Updated.', $type);
 };
