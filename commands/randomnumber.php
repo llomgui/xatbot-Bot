60553ef15f1763d0349374df989cb0326c3f6f61
Guillaume Massard <llomgui@live.fr>
2017-04-04 07:07:09 +0000

    Handle 64-bit integer

--- commands/randomnumber.php
+++ commands/randomnumber.php
@@ -11,7 +11,11 @@ $randomnumber = function (int $who, array $message, int $type) {
     if (!isset($message[1]) || empty($message[1]) || !is_numeric($message[1])) {
         return $bot->network->sendMessageAutoDetection($who, 'Usage: !randomnumber [maxNumber]', $type, true);
     } else {
-        $maxNumber = $message[1];
+        $maxNumber = (int)$message[1];
+    }
+
+    if ($maxNumber == 9223372036854775807) {
+        return $bot->network->sendMessageAutoDetection($who, 'I am in a 64-bit environment.', $type, true);
     }
 
     $number = rand(0, $maxNumber);
