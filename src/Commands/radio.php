fc6feeeba5735e36cf0fc6ff45c04c7201ccdb1b
Guillaume Massard <llomgui@live.fr>
2017-07-24 23:27:56 +0000

    Adding radio command #2

---
+++ src/Commands/radio.php
@@ -0,0 +1,37 @@
+<?php
+
+use OceanProject\API\DataAPI;
+
+$radio = function (int $who, array $message, int $type) {
+
+    $bot  = OceanProject\API\ActionAPI::getBot();
+
+    if (!$bot->minrank($who, 'radio')) {
+        return $bot->network->sendMessageAutoDetection($who, $bot->botlang('not.enough.rank'), $type);
+    }
+
+    if (DataAPI::isSetVariable('radio')) {
+        $infos = DataAPI::get('radio');
+        if ($infos['lastCheck'] > time()) {
+            return $bot->network->sendMessageAutoDetection(
+                $who,
+                'Listening to: ' . $infos['song'] . ' ' . $infos['listeners'] . '/' . $infos['max'] . '.',
+                $type
+            );
+        }
+    }
+
+    $song = $bot->getCurrentSong();
+
+    if ($song == false) {
+        $this->network->sendMessageAutoDetection($who, 'You have an error with the radio!', $who);
+    }
+
+    DataAPI::set('radio', $song);
+
+    return $bot->network->sendMessageAutoDetection(
+        $who,
+        'Listening to: ' . $song['song'] . ' ' . $song['listeners'] . '/' . $song['max'] . '.',
+        $type
+    );
+};
