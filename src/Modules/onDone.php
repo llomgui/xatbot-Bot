4d257318b0174aa75e6ea86746ab81e96776a353
xSlOom <illuclem@gmail.com>
2017-09-23 15:04:02 +0200

    Added autoguestself

--- src/Modules/onDone.php
+++ src/Modules/onDone.php
@@ -5,4 +5,13 @@ $onDone = function (array $array) {
     $bot = OceanProject\API\ActionAPI::getBot();
     $bot->network->idleTime = time();
     $bot->done = true;
+
+    if ($bot->data->was_connected == false) {
+        $bot->network->sendMessage(
+            '/g'
+        );
+
+        $bot->data->was_connected = true;
+        $bot->data->save();
+    }
 };
