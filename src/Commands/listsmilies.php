bb62400013b46a67281a102190180f533cb8e9ab
llomgui <llomgui@live.fr>
2018-01-15 17:28:48 +0100

    Adding all feature for listsmilies command

--- src/Commands/listsmilies.php
+++ src/Commands/listsmilies.php
@@ -31,10 +31,45 @@ $listsmilies = function (int $who, array $message, int $type) {
             $topsh = $array['smilies'];
 
             if (count($topsh) > 10) {
-                $implode = $bot->botHasPower($id) ?
-                    '('.implode('#)(', array_slice($topsh, 0, 10)).'#)' :
-                    implode(' ', array_slice($topsh, 0, 10));
-                $implode .= implode(' ', array_slice($topsh, 10));
+                if (isset($message[2]) && strtolower($message[2]) == 'all') {
+                    $nbMessages = ceil(sizeof($topsh) / 10);
+
+                    if (sizeof($bot->packetsinqueue) > 0) {
+                        $bot->packetsinqueue[max(array_keys($bot->packetsinqueue)) + 1000] = [
+                            'who' => $who,
+                            'message' => '('.implode('#)(', array_slice($topsh, 0, 10)).'#)',
+                            'type' => $type
+                        ];
+                    } else {
+                        $bot->packetsinqueue[round(microtime(true) * 1000) + 1000] = [
+                            'who' => $who,
+                            'message' => '('.implode('#)(', array_slice($topsh, 0, 10)).'#)',
+                            'type' => $type
+                        ];
+                    }
+
+                    for ($i = 10; $i < ($nbMessages * 10); $i += 10) {
+                        if (sizeof($bot->packetsinqueue) > 0) {
+                            $bot->packetsinqueue[max(array_keys($bot->packetsinqueue)) + 1000] = [
+                                'who' => $who,
+                                'message' => '('.implode('#)(', array_slice($topsh, $i, 10)).'#)',
+                                'type' => $type
+                            ];
+                        } else {
+                            $bot->packetsinqueue[round(microtime(true) * 1000) + 1000] = [
+                                'who' => $who,
+                                'message' => '('.implode('#)(', array_slice($topsh, $i, 10)).'#)',
+                                'type' => $type
+                            ];
+                        }
+                    }
+                    return;
+                } else {
+                    $implode = $bot->botHasPower($id) ?
+                        '('.implode('#)(', array_slice($topsh, 0, 10)).'#)' :
+                        implode(' ', array_slice($topsh, 0, 10));
+                    $implode .= implode(' ', array_slice($topsh, 10));
+                }
             } else {
                 $implode = $bot->botHasPower($id) ? '(' . implode('#)(', $topsh) . '#)' : implode(' ', $topsh);
             }
