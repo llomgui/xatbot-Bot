122994cec4b371fdb8bedb15cc16c2e5ad82b353
llomgui <llomgui@live.fr>
2018-01-23 15:11:23 +0000

    tempfix until admin fix j2 packet

--- src/Modules/onIdle.php
+++ src/Modules/onIdle.php
@@ -1,6 +1,14 @@
 <?php
 
-$onIdle = function () {
+$onIdle = function (array $array) {
     $bot = xatbot\API\ActionAPI::getBot();
-    $bot->network->reconnect();
+
+    // tempfix to avoid idle (j2 packet has too many attributes to connect)
+    if (isset($array['e']) && $array['e'] == 'I04') {
+        $bot->data->chatpw = null;
+        $bot->data->save();
+        $bot->refresh();
+    } else {
+        $bot->network->reconnect();
+    }
 };
