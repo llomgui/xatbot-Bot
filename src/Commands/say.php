7122638318a9ca19b344ba3562bd421567f5885b
llomgui <llomgui@live.fr>
2018-01-29 10:50:03 +0000

    Adding a mini-feature for say command

--- src/Commands/say.php
+++ src/Commands/say.php
@@ -15,6 +15,14 @@ $say = function (int $who, array $message, int $type) {
         return $bot->network->sendMessageAutoDetection($who, $bot->botlang('message.cannotbeempty'), $type);
     } else {
         //$message = str_replace('/', '_/', $message);
-        return $bot->network->sendMessageAutoDetection($who, '_' . $message, $type);
+        if ($type != 3) {
+            return $bot->network->sendMessageAutoDetection($who, '_' . $message, $type);
+        } else {
+            if ($bot->flagToRank($who) < $bot->stringToRank('mod')) {
+                return $bot->network->sendMessage('[' . $who . '] ' . $message);
+            } else {
+                return $bot->network->sendMessage('_' . $message);
+            }
+        }
     }
 };
